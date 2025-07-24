7f0fdab9ac37f26cdbb4dc48af268af85819d8bb
Iara Moreiras Franco <121463061+iaragab@users.noreply.github.com>
2025-07-24 21:18:00 +0200

    agregando archivos y imagenes

---
+++ portafolio.js
@@ -0,0 +1,71 @@
+// menu 
+
+const btnMenu = document.getElementById("menuExpenBTN")
+const ulMenu = document.getElementById("menuUl")
+
+
+btnMenu.addEventListener("click", function(){
+    ulMenu.classList.toggle("show")
+})
+
+
+
+// galeria
+
+const imagenes = [
+    {
+        id : 1,
+        img : '../portafolio/img-portafolio/1E64C38D-C90A-4B5C-BC5F-ED20F502D015_1_201_a.jpeg',
+    },
+    {
+        id : 1,
+        img : '../portafolio/img-portafolio/fondo5.png',
+    },
+    {
+        id : 1,
+        img : '../portafolio/img-portafolio/fondo3.png',
+    },
+    {
+        id : 1,
+        img : '../portafolio/img-portafolio/fondo1.png',
+    }
+]
+
+// llamadas
+
+const img = document.getElementById("gale-img")
+
+const prevbtn = document.querySelector(".prev-btn")
+const newtbtn = document.querySelector(".next-btn")
+
+let currenItem = 0
+
+// funcion
+
+function showImag(index){
+    const item = imagenes[index]
+    img.src = item.img
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    showImag(currenItem)
+})
+
+// botones
+
+newtbtn.addEventListener("click", function(){
+    currenItem++
+    if (currenItem > imagenes.length -1) {
+        currenItem = 0
+    }
+    showImag(currenItem)
+})
+
+prevbtn.addEventListener("click", function(){
+    currenItem--
+    if (currenItem < 0) {
+        currenItem = imagenes.length -1
+    }
+    showImag(currenItem)
+})
+
