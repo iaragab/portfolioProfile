e6ba41bbda163c01bf3a124edc7f1205378cea73
Iara Moreiras Franco <121463061+iaragab@users.noreply.github.com>
2025-08-27 12:20:31 +0200

    cambios en la maqueta

--- guardados.js
+++ guardados.js
@@ -1,21 +1,28 @@
 const body = document.body;
+const mainChance =document.getElementById("main");
 const btnDark = document.getElementById("moon-sun");
 const logoNav = document.getElementById("logo");
-const headerDark = document.getElementById("dark-header")
+const headerDark = document.getElementById("dark-header");
 const title = document.getElementById("title-chance");
-const linkUl = document.querySelectorAll(".a-chance")
+const linkUl = document.querySelectorAll(".a-chance");
+const contact = document.getElementById("contact")
 
 
 // Recuperar configuración guardada
   if (localStorage.getItem("modoBodyDark") === "oscuro") {
     body.classList.add("body-dark");
-}
+  }
+
+  if (localStorage.getItem("modoMainDark") === "oscuro") {
+    body.classList.add("main-dark");
+  }
+
   if (localStorage.getItem("modoSun") === "activo"){
     btnDark.classList.add("botonSun");
-}
+  }
   if (localStorage.getItem("modoTitle") === "activo") {
     title.classList.add("title-black");
-}
+  }
 
   if (localStorage.getItem("modoHeader") === "activo") {
     headerDark.classList.add("headerBlack")
@@ -23,24 +30,37 @@ const linkUl = document.querySelectorAll(".a-chance")
 
   if (localStorage.getItem("modoLogo") === "activo") {
     logoNav.classList.add("logoImgdark");
-}
-if (localStorage.getItem("modolinks") === "activos") {
-    linkUl.forEach(linkA => linkA.classList.add("linkA"))
-}
+  }
+
+  if (localStorage.getItem("modoContact") === "activos") {
+      linkUl.forEach(linkA => linkA.classList.add("contact-dark"))
+  }
+
+  if (localStorage.getItem("modolinks") === "activos") {
+      linkUl.forEach(linkA => linkA.classList.add("linkA"))
+  }
 
 // Función del botón
 
 btnDark.addEventListener("click", () => {
    body.classList.toggle("body-dark") 
+   mainChance.classList.toggle("imgMain")
     btnDark.classList.toggle("botonSun");
     title.classList.toggle("title-black")
     logoNav.classList.toggle("logoImgdark")
     headerDark.classList.toggle("headerBlack")
+    contact.classList.toggle("contact-dark")
     linkUl.forEach(linkA => linkA.classList.toggle("linkA"));
 
 
     if (body.classList.contains("body-dark")) {
     localStorage.setItem("modoBodyDark", "oscuro");
+    } else {
+    localStorage.removeItem("modoBodyDark");
+    }
+
+    if (body.classList.contains("imgMain")) {
+    localStorage.setItem("modoMainDark", "oscuro");
     } else {
     localStorage.removeItem("modoBodyDark");
     }
@@ -65,10 +85,15 @@ btnDark.addEventListener("click", () => {
 
   if (logoNav.classList.contains("logoImgdark")) {
   localStorage.setItem("modoLogo", "activo"); 
-} else {
+  } else {
   localStorage.removeItem("modoLogo");
 }
 
+    if (title.classList.contains("contact-dark")) {
+    localStorage.setItem("modoContact", "activo");
+  } else {
+    localStorage.removeItem("modoTitle");
+  }
 
  const chanceLink = [...linkUl].some(linkA =>
     linkA.classList.contains("linkA"));
